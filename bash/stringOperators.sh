58855218d573d886bb075536e84f8798d30bbe88
nitendragautam <nitendra.gautam12@gmail.com>
2022-04-11 18:28:58 -0500

    Adding Docker Images on Spark

--- bash/stringOperators.sh
+++ bash/stringOperators.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-str1=""
-str2="Nitendra"
-str3="Gautam"
+## String Manipulation
+str1=nitendratech
+echo $str1
+
+str2="nitendratech"
+echo $str2
+
+str3='nitendragautam'
+echo $str3
 
 # Check if String is empty or not
 if [ "$str1" ]; then
-    echo "$str1 is not null"
+    echo "Value of str1: $str1 is not null"
 fi
 
 #Check if String has value or not
 if [[ -z "$str1" ]]; then
-    echo "$str1 has no value"
+    echo "Value of str1: $str1 has no value"
+else
+    echo "Value of str1: $str1 has Certain value"
 fi
 
 # Comparing two String values
+
 if [[ "$str2" = "$str3" ]]; then
     echo "$str2 equals $str3 "
 elif [[ "$str2" != "$str3" ]]; then
